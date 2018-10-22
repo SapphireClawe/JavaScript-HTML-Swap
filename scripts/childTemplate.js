107a862cdc477f79337df9226875e8d75d3592e0
SapphireClawe <37085710+SapphireClawe@users.noreply.github.com>
2018-10-22 12:52:58 -0600

    u

--- scripts/childTemplate.js
+++ scripts/childTemplate.js
@@ -1,1 +1,25 @@
-console.log("You made it to the childTemplate.js!");
+console.log("I made it to childTemplate.js!");
+
+function main () {
+
+  let firstNumber, secondNumber, text;
+
+  firstNumber = document.getElementById("textfield1").value;
+
+  document.getElementById("validityTest1").innerHTML = alert( testNaN (firstNumber));
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+
+  secondNumber = document.getElementById("textfield2").value;
+
+  document.getElementById("validityTest2").innerHTML = alert( testNaN (secondNumber));
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+
+  if (stop == true) {
+    document.getElementById("large").innerHTML = "Please restart"
+    else
+
+  }
+
+
+
+}
